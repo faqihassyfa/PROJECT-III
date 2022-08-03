26c6d85f5ab509ad954bce22171e96cfab3f8ce0
Cindy05-shintia <Csinthia10@gmail.com>
2022-08-03 14:11:02 +0700

    installer mockery

--- domain/Admin.go
+++ domain/Admin.go
@@ -1,1 +1,31 @@
 package domain
+
+import "time"
+
+type Admin struct {
+	ID        int
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Clothes struct {
+	ID        int
+	Name      string
+	Price     int
+	Stock     int
+	Image     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type AdminUseCase interface {
+	CreateProducts(data Clothes)
+	GetHistory([]Clothes, int)
+}
+
+type AdminData interface {
+	GetHistoryData() []Clothes
+}
